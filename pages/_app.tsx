c78a44546a667b19de74da2ececf6f1dc50b43a2
nolbuzanis <nolanbuzanis@gmail.com>
2021-06-04 13:42:14 -0400

    feat: normalize css, more global styles, and set up _app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import styled from 'styled-components';
 import { AppProps } from 'next/app';
 import GlobalFonts from '../fonts/fonts';
+// import '../styles/normalize.css';
 import '../styles/globals.css';
+
+const Content = styled.main`
+  max-width: 1140px;
+  margin: 0 auto;
+`;
+
+const Background = styled.div`
+  background-color: #fafafa;
+  padding: 0 10%;
+`;
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <>
-      <GlobalFonts />
-      <Component {...pageProps} />
-    </>
+    <Background>
+      <Content>
+        <GlobalFonts />
+        <Component {...pageProps} />
+      </Content>
+    </Background>
   );
 }
 
