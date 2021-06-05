4d10baf476816717bef4e074bd670fc93bda1c33
nolbuzanis <nolanbuzanis@gmail.com>
2021-06-05 15:59:27 -0400

    feat: created theme icon

---
+++ components/icons/theme-icon.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/indent */
+import styled from 'styled-components';
+
+const ThemeButton = styled.button`
+  opacity: 0.5;
+  position: relative;
+  border-radius: 5px;
+  width: 40px;
+  height: 25px;
+  transition: opacity 0.25s ease;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  &: hover {
+    opacity: 1;
+  }
+`;
+
+const MainCircle = styled.div`
+  position: relative;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  border: ${(props) => (props.theme.name === 'dark' ? '4px solid #fff' : '2px solid #000')};
+  background: ${(props) => props.theme.textColor};
+  transform: ${(props) => (props.theme.name === 'light' ? 'scale(1)' : 'scale(0.55)')};
+  transition: all 0.45s ease;
+  overflow: ${(props) => (props.theme.name === 'light' ? 'hidden' : 'visible')};
+
+  &: after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    margin: -4px 0px 0px -4px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: ${(props) => (props.theme.name === 'dark' ? 'scale(1)' : 'scale(0)')};
+    transition: all 0.35s ease 0s;
+    ${(props) => {
+      const { textColor } = props.theme;
+      return `box-shadow: 0 -23px 0 ${textColor}, 0 23px 0 ${textColor}, 23px 0 0 ${textColor}, -23px 0 0 ${textColor}, 15px 15px 0 ${textColor},
+      -15px 15px 0 ${textColor}, 15px -15px 0 ${textColor}, -15px -15px 0 ${textColor};`;
+    }}
+  }
+`;
+
+const WhiteCircle = styled.div`
+  position: absolute;
+  height: 24px;
+  width: 24px;
+  border-radius: 50%;
+  border: 0;
+  background: ${(props) => props.theme.bgColor}};
+  transform: translate(${(props) => (props.theme.name === 'dark' ? '14px, -14px' : '0, 0')});
+  opacity: ${(props) => (props.theme.name === 'dark' ? '0' : '1')};
+  right: -1px;
+  top: -8px;
+  transition: background 0.25s ease, color 0.25s ease, transform 0.45s ease;
+  overflow: visible;
+`;
+
+interface ThemeIconProps {
+  onClick: () => void;
+}
+
+const ThemeIcon = ({ onClick }: ThemeIconProps): JSX.Element => (
+  <ThemeButton onClick={onClick}>
+    <MainCircle />
+    <WhiteCircle />
+  </ThemeButton>
+);
+
+export default ThemeIcon;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import styled from 'styled-components';
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 import { AppProps } from 'next/app';
 import GlobalFonts from '../fonts/fonts';
-// import '../styles/normalize.css';
+import '../styles/normalize.css';
 import '../styles/globals.css';
+import { lightTheme, darkTheme } from '../utils/theme';
+import Header from '../components/header';
 
 const Content = styled.main`
   max-width: 1140px;
@@ -11,21 +14,37 @@ const Content = styled.main`
 `;
 
 const Background = styled.div`
-  background-color: #fafafa;
-  padding: 0 8%;
+  background-color: ${(props) => props.theme.bgColor};
+  padding: 0 4rem;
+  transition: background 0.25s ease, color 0.25s ease;
+  min-height: 100vh;
   @media only screen and (max-width: 540px) {
     padding: 0 20px;
   }
 `;
 
+const mapThemeName = {
+  dark: darkTheme,
+  light: lightTheme,
+};
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const handleThemeChange = (themeName: 'dark' | 'light') => {
+    setTheme(mapThemeName[themeName]);
+  };
+
   return (
-    <Background>
-      <Content>
-        <GlobalFonts />
-        <Component {...pageProps} />
-      </Content>
-    </Background>
+    <ThemeProvider theme={theme}>
+      <Background>
+        <Header setTheme={handleThemeChange} />
+        <Content>
+          <GlobalFonts />
+          <Component {...pageProps} />
+        </Content>
+      </Background>
+    </ThemeProvider>
   );
 }
 
